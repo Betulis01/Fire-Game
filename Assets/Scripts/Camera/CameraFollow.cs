1bb001d7801675a111f7c744357fab96de05ae04
Betulis01 <Emil.birchxx@hotmail.com>
2026-06-16 18:19:46 +0200

    Camera follow

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;        // the player
+    public Vector3 offset = new Vector3(0, 0, -10);
+
+    void LateUpdate()
+    {
+        if (target != null)
+            transform.position = target.position + offset;
+    }
+}
